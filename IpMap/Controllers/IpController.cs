1cf87d6fa9bfce9b3c3d669aadca212ff969c31a
natellu <t.le0nh4rdt@gmail.com>
2021-03-25 13:17:06 +0100

    f

---
+++ IpMap/Controllers/IpController.cs
@@ -0,0 +1,23 @@
+﻿using IpMap.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IpMap.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IpController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IpDataModel> Get()
+        {
+            IpDataModel data = new IpDataModel();
+            data.IP = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            return Ok(data);
+        }
+    }
+}
---
+++ IpMap/Models/IpDataModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IpMap.Models
+{
+    public class IpDataModel
+    {
+        public string IP { get; set; }
+    }
+}
